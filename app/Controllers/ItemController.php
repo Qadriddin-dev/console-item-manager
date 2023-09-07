e3648efd67ab0e71c9c8197423c27417e8c321ee
Qadriddin <qadrdin81@gmail.com>
2023-09-07 14:52:54 +0500

    added CRUD and total amount logic for items

---
+++ app/Controllers/ItemController.php
@@ -0,0 +1,36 @@
+<?php
+class ItemController
+{
+    private $itemList;
+
+    public function __construct(ItemList $itemList)
+    {
+        $this->itemList = $itemList;
+    }
+
+    public function add($name, $price)
+    {
+        $item = new Item($name, $price);
+        $this->itemList->addItem($item);
+    }
+
+    public function edit($name, $newPrice)
+    {
+        $this->itemList->editItem($name, $newPrice);
+    }
+
+    public function delete($name)
+    {
+        $this->itemList->deleteItem($name);
+    }
+
+    public function getTotal()
+    {
+        return $this->itemList->getTotalPrice();
+    }
+
+    public function displayItems()
+    {
+        $this->itemList->displayItems();
+    }
+}
