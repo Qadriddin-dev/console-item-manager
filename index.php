54b2852b5f85764af234d2bce0808095096ccb1a
Qadriddin <qadrdin81@gmail.com>
2023-09-07 14:55:03 +0500

    The main entry point of the console application

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'app/Controllers/ItemController.php';
+require_once 'app/Controllers/ConsoleController.php';
+require_once 'app/Models/Item.php';
+require_once 'app/Models/ItemList.php';
+
+if ($argc < 3) {
+    echo "Использование: php index.php <файл> <действие> [аргументы]\n";
+    exit(1);
+}
+
+$filename = 'data/' . $argv[1];
+$action = $argv[2];
+$args = array_slice($argv, 3);
+
+$itemList = new ItemList();
+$itemController = new ItemController($itemList);
+$consoleController = new ConsoleController($itemController, $itemList);
+$consoleController->execute($filename, $action, $args);
+
+echo "Текущий список товаров:" . PHP_EOL;
+$itemList->displayItems();
+
