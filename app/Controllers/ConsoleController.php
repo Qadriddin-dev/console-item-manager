603bd0d56e1f038cd4695aba277aba66e4f3fa29
Qadriddin <qadrdin81@gmail.com>
2023-09-07 14:50:27 +0500

    added Console Controller for console command

---
+++ app/Controllers/ConsoleController.php
@@ -0,0 +1,38 @@
+<?php
+class ConsoleController
+{
+    private $itemController;
+    private $itemList;
+
+    public function __construct(ItemController $itemController, ItemList $itemList)
+    {
+        $this->itemController = $itemController;
+        $this->itemList = $itemList;
+    }
+
+    public function execute($filename, $action, $args)
+    {
+        $this->itemList->loadItemsFromFile($filename);
+
+        switch ($action) {
+            case 'add':
+                $this->itemController->add($args[0], $args[1]);
+                break;
+            case 'edit':
+                $this->itemController->edit($args[0], $args[1]);
+                break;
+            case 'delete':
+                $this->itemController->delete($args[0]);
+                break;
+            case 'subtract':
+                $total = $this->itemController->getTotal();
+                echo "Общая сумма товаров: $total" . PHP_EOL;
+                break;
+            default:
+                echo "Неправильное действие." . PHP_EOL;
+                break;
+        }
+
+        $this->itemList->saveItemsToFile($filename);
+    }
+}
